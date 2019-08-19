6c1a4f76a81b7a45a845dbe7e1eb06ff14c1e521
jingjiangtao <15210717655@163.com>
2019-08-19 20:57:19 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace CaptureVideo
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.videoPathTxtBox = new System.Windows.Forms.TextBox();
+			this.saveDirTxtBox = new System.Windows.Forms.TextBox();
+			this.SelectSaveDirBtn = new System.Windows.Forms.Button();
+			this.CaptureVideoFrame = new System.Windows.Forms.Button();
+			this.message = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(462, 38);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(94, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "选择";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.SelectVideoPath);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(0, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 0;
+			// 
+			// videoPathTxtBox
+			// 
+			this.videoPathTxtBox.Location = new System.Drawing.Point(39, 38);
+			this.videoPathTxtBox.Name = "videoPathTxtBox";
+			this.videoPathTxtBox.Size = new System.Drawing.Size(391, 21);
+			this.videoPathTxtBox.TabIndex = 1;
+			this.videoPathTxtBox.Text = "选择视频";
+			// 
+			// saveDirTxtBox
+			// 
+			this.saveDirTxtBox.Location = new System.Drawing.Point(39, 109);
+			this.saveDirTxtBox.Name = "saveDirTxtBox";
+			this.saveDirTxtBox.Size = new System.Drawing.Size(391, 21);
+			this.saveDirTxtBox.TabIndex = 2;
+			this.saveDirTxtBox.Text = "选择保存图片的目录";
+			// 
+			// SelectSaveDirBtn
+			// 
+			this.SelectSaveDirBtn.Location = new System.Drawing.Point(462, 106);
+			this.SelectSaveDirBtn.Name = "SelectSaveDirBtn";
+			this.SelectSaveDirBtn.Size = new System.Drawing.Size(94, 23);
+			this.SelectSaveDirBtn.TabIndex = 3;
+			this.SelectSaveDirBtn.Text = "选择";
+			this.SelectSaveDirBtn.UseVisualStyleBackColor = true;
+			this.SelectSaveDirBtn.Click += new System.EventHandler(this.SelectSaveDirBtn_Click);
+			// 
+			// CaptureVideoFrame
+			// 
+			this.CaptureVideoFrame.Location = new System.Drawing.Point(156, 190);
+			this.CaptureVideoFrame.Name = "CaptureVideoFrame";
+			this.CaptureVideoFrame.Size = new System.Drawing.Size(274, 23);
+			this.CaptureVideoFrame.TabIndex = 4;
+			this.CaptureVideoFrame.Text = "生成视频帧图片";
+			this.CaptureVideoFrame.UseVisualStyleBackColor = true;
+			this.CaptureVideoFrame.Click += new System.EventHandler(this.CaptureVideoFrame_Click);
+			// 
+			// message
+			// 
+			this.message.AutoSize = true;
+			this.message.Location = new System.Drawing.Point(264, 235);
+			this.message.Name = "message";
+			this.message.Size = new System.Drawing.Size(0, 12);
+			this.message.TabIndex = 5;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(594, 266);
+			this.Controls.Add(this.message);
+			this.Controls.Add(this.CaptureVideoFrame);
+			this.Controls.Add(this.SelectSaveDirBtn);
+			this.Controls.Add(this.saveDirTxtBox);
+			this.Controls.Add(this.videoPathTxtBox);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox videoPathTxtBox;
+		private System.Windows.Forms.TextBox saveDirTxtBox;
+		private System.Windows.Forms.Button SelectSaveDirBtn;
+		private System.Windows.Forms.Button CaptureVideoFrame;
+		private System.Windows.Forms.Label message;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using Emgu.CV;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaptureVideo
+{
+	public partial class Form1 : Form
+	{
+		private string videoPath = string.Empty;
+		private string saveDir = string.Empty;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void SelectVideoPath(object sender, EventArgs e)
+		{
+			OpenFileDialog openFileDialog1 = new OpenFileDialog();     //显示选择文件对话框
+			openFileDialog1.InitialDirectory = "c:\\";
+			openFileDialog1.Filter = "All files (*.*)|*.*|video files|*.mp4;*.wmv";
+			openFileDialog1.FilterIndex = 2;
+			openFileDialog1.RestoreDirectory = true;
+
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				this.videoPath = openFileDialog1.FileName;
+				this.videoPathTxtBox.Text = openFileDialog1.FileName;          //显示文件路径
+			}
+		}
+
+		private void SelectSaveDirBtn_Click(object sender, EventArgs e)
+		{
+			FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+			folderBrowserDialog1.SelectedPath = "C:\\";
+			if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+			{
+				this.saveDir = folderBrowserDialog1.SelectedPath;
+				this.saveDirTxtBox.Text = folderBrowserDialog1.SelectedPath;
+			}
+		}
+
+		private async void CaptureVideoFrame_Click(object sender, EventArgs e)
+		{
+
+			try
+			{
+				if (videoPath == string.Empty || saveDir == string.Empty)
+				{
+					return;
+				}
+				Task task = Task.Run(() => CaptureVideo(videoPath, saveDir));
+				this.message.Text = "处理中...";
+				
+				await task;
+				this.message.Text = "完成";
+			}catch
+			{
+				this.message.Text = "失败";
+				return;
+			}
+		}
+		private static void CaptureVideo(string video, string saveDir)
+		{
+			using (VideoCapture capture = new VideoCapture(video))
+			{
+				if (!capture.IsOpened)
+				{
+					throw new Exception($"视频 [{video}] 打开失败");
+				}
+
+				int index = 0;
+				using (Mat mat = new Mat())
+				{
+					capture.Read(mat);
+					while (!mat.IsEmpty)
+					{
+						mat.Bitmap.Save($"{System.IO.Path.Combine(saveDir, index.ToString())}.bmp");
+						capture.Read(mat);
+						index++;
+					}
+				}
+				Console.WriteLine("视频读取完毕");
+
+			}
+		}
+	}
+}
