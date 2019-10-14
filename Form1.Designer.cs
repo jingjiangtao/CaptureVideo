fb30b029930dd2d6ccdb012b4783f8309b6ba6a1
jingjiangtao <15210717655@163.com>
2019-10-14 17:27:04 +0800

    添加下拉框选择图片格式控件；修改字体

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,94 +28,118 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.button1 = new System.Windows.Forms.Button();
-			this.label1 = new System.Windows.Forms.Label();
-			this.videoPathTxtBox = new System.Windows.Forms.TextBox();
-			this.saveDirTxtBox = new System.Windows.Forms.TextBox();
-			this.SelectSaveDirBtn = new System.Windows.Forms.Button();
-			this.CaptureVideoFrame = new System.Windows.Forms.Button();
-			this.message = new System.Windows.Forms.Label();
-			this.SuspendLayout();
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(462, 38);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(94, 23);
-			this.button1.TabIndex = 0;
-			this.button1.Text = "选择";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.SelectVideoPath);
-			// 
-			// label1
-			// 
-			this.label1.Location = new System.Drawing.Point(0, 0);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(100, 23);
-			this.label1.TabIndex = 0;
-			// 
-			// videoPathTxtBox
-			// 
-			this.videoPathTxtBox.Location = new System.Drawing.Point(39, 38);
-			this.videoPathTxtBox.Name = "videoPathTxtBox";
-			this.videoPathTxtBox.ReadOnly = true;
-			this.videoPathTxtBox.Size = new System.Drawing.Size(391, 21);
-			this.videoPathTxtBox.TabIndex = 1;
-			this.videoPathTxtBox.Text = "选择视频";
-			// 
-			// saveDirTxtBox
-			// 
-			this.saveDirTxtBox.Location = new System.Drawing.Point(39, 109);
-			this.saveDirTxtBox.Name = "saveDirTxtBox";
-			this.saveDirTxtBox.ReadOnly = true;
-			this.saveDirTxtBox.Size = new System.Drawing.Size(391, 21);
-			this.saveDirTxtBox.TabIndex = 2;
-			this.saveDirTxtBox.Text = "选择保存图片的目录";
-			// 
-			// SelectSaveDirBtn
-			// 
-			this.SelectSaveDirBtn.Location = new System.Drawing.Point(462, 106);
-			this.SelectSaveDirBtn.Name = "SelectSaveDirBtn";
-			this.SelectSaveDirBtn.Size = new System.Drawing.Size(94, 23);
-			this.SelectSaveDirBtn.TabIndex = 3;
-			this.SelectSaveDirBtn.Text = "选择";
-			this.SelectSaveDirBtn.UseVisualStyleBackColor = true;
-			this.SelectSaveDirBtn.Click += new System.EventHandler(this.SelectSaveDirBtn_Click);
-			// 
-			// CaptureVideoFrame
-			// 
-			this.CaptureVideoFrame.Location = new System.Drawing.Point(156, 190);
-			this.CaptureVideoFrame.Name = "CaptureVideoFrame";
-			this.CaptureVideoFrame.Size = new System.Drawing.Size(274, 23);
-			this.CaptureVideoFrame.TabIndex = 4;
-			this.CaptureVideoFrame.Text = "生成视频帧图片";
-			this.CaptureVideoFrame.UseVisualStyleBackColor = true;
-			this.CaptureVideoFrame.Click += new System.EventHandler(this.CaptureVideoFrame_Click);
-			// 
-			// message
-			// 
-			this.message.AutoSize = true;
-			this.message.Location = new System.Drawing.Point(264, 235);
-			this.message.Name = "message";
-			this.message.Size = new System.Drawing.Size(0, 12);
-			this.message.TabIndex = 5;
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(594, 266);
-			this.Controls.Add(this.message);
-			this.Controls.Add(this.CaptureVideoFrame);
-			this.Controls.Add(this.SelectSaveDirBtn);
-			this.Controls.Add(this.saveDirTxtBox);
-			this.Controls.Add(this.videoPathTxtBox);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.button1);
-			this.Name = "Form1";
-			this.Text = "Form1";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.videoPathTxtBox = new System.Windows.Forms.TextBox();
+            this.saveDirTxtBox = new System.Windows.Forms.TextBox();
+            this.SelectSaveDirBtn = new System.Windows.Forms.Button();
+            this.CaptureVideoFrame = new System.Windows.Forms.Button();
+            this.message = new System.Windows.Forms.Label();
+            this.saveImgFormat = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(539, 54);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.SelectVideoPath);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 33);
+            this.label1.TabIndex = 0;
+            // 
+            // videoPathTxtBox
+            // 
+            this.videoPathTxtBox.Location = new System.Drawing.Point(45, 54);
+            this.videoPathTxtBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.videoPathTxtBox.Name = "videoPathTxtBox";
+            this.videoPathTxtBox.ReadOnly = true;
+            this.videoPathTxtBox.Size = new System.Drawing.Size(455, 23);
+            this.videoPathTxtBox.TabIndex = 1;
+            this.videoPathTxtBox.Text = "选择视频";
+            // 
+            // saveDirTxtBox
+            // 
+            this.saveDirTxtBox.Location = new System.Drawing.Point(45, 118);
+            this.saveDirTxtBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.saveDirTxtBox.Name = "saveDirTxtBox";
+            this.saveDirTxtBox.ReadOnly = true;
+            this.saveDirTxtBox.Size = new System.Drawing.Size(455, 23);
+            this.saveDirTxtBox.TabIndex = 2;
+            this.saveDirTxtBox.Text = "选择保存图片的目录";
+            // 
+            // SelectSaveDirBtn
+            // 
+            this.SelectSaveDirBtn.Location = new System.Drawing.Point(539, 114);
+            this.SelectSaveDirBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.SelectSaveDirBtn.Name = "SelectSaveDirBtn";
+            this.SelectSaveDirBtn.Size = new System.Drawing.Size(110, 27);
+            this.SelectSaveDirBtn.TabIndex = 3;
+            this.SelectSaveDirBtn.Text = "选择";
+            this.SelectSaveDirBtn.UseVisualStyleBackColor = true;
+            this.SelectSaveDirBtn.Click += new System.EventHandler(this.SelectSaveDirBtn_Click);
+            // 
+            // CaptureVideoFrame
+            // 
+            this.CaptureVideoFrame.Location = new System.Drawing.Point(539, 183);
+            this.CaptureVideoFrame.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.CaptureVideoFrame.Name = "CaptureVideoFrame";
+            this.CaptureVideoFrame.Size = new System.Drawing.Size(110, 25);
+            this.CaptureVideoFrame.TabIndex = 4;
+            this.CaptureVideoFrame.Text = "生成";
+            this.CaptureVideoFrame.UseVisualStyleBackColor = true;
+            this.CaptureVideoFrame.Click += new System.EventHandler(this.CaptureVideoFrame_Click);
+            // 
+            // message
+            // 
+            this.message.AutoSize = true;
+            this.message.Location = new System.Drawing.Point(308, 244);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(0, 17);
+            this.message.TabIndex = 5;
+            // 
+            // saveImgFormat
+            // 
+            this.saveImgFormat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.saveImgFormat.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.saveImgFormat.FormattingEnabled = true;
+            this.saveImgFormat.Items.AddRange(new object[] {
+            "jpg",
+            "png",
+            "bmp"});
+            this.saveImgFormat.Location = new System.Drawing.Point(45, 183);
+            this.saveImgFormat.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.saveImgFormat.Name = "saveImgFormat";
+            this.saveImgFormat.Size = new System.Drawing.Size(455, 25);
+            this.saveImgFormat.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 310);
+            this.Controls.Add(this.saveImgFormat);
+            this.Controls.Add(this.message);
+            this.Controls.Add(this.CaptureVideoFrame);
+            this.Controls.Add(this.SelectSaveDirBtn);
+            this.Controls.Add(this.saveDirTxtBox);
+            this.Controls.Add(this.videoPathTxtBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "生成视频帧";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
@@ -128,6 +152,7 @@
 		private System.Windows.Forms.Button SelectSaveDirBtn;
 		private System.Windows.Forms.Button CaptureVideoFrame;
 		private System.Windows.Forms.Label message;
-	}
+        private System.Windows.Forms.ComboBox saveImgFormat;
+    }
 }
 
